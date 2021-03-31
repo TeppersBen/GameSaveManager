b59e001b3ab721eab65c5ba7ec27d0673d42b927
Ben Teppers <ben.teppers@icloud.com>
2021-03-31 10:03:23 +0200

    Added base Factorio Frames

---
+++ src/main/java/com/frames/factorio/FactorioSaveFolderFrame.java
@@ -0,0 +1,10 @@
+package com.frames.factorio;
+
+import com.frames.core.SaveFolderFrame;
+
+public class FactorioSaveFolderFrame extends SaveFolderFrame {
+    @Override
+    public void refreshContent() {
+
+    }
+}

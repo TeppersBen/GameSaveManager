d667fb4aec313673ff61f0ed499c8fe698016635
Ben Teppers <ben.teppers@icloud.com>
2021-08-08 20:07:55 +0200

    AddGameFrame layout change + code cleanup

--- src/main/java/com/frames/core/AddGameFrame.java
+++ src/main/java/com/frames/core/AddGameFrame.java
@@ -1,37 +1,47 @@
 package com.frames.core;
 
-import com.frames.core.Frame;
 import com.frames.factorio.FactorioMainFrame;
 import com.frames.minecraft.MinecraftMainFrame;
 import com.frames.rimworld.RimworldMainFrame;
 import com.frames.satisfactory.SatisfactoryMainFrame;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
-import com.managers.IOManager;
 import com.managers.PropertiesManager;
 import javafx.geometry.HPos;
-import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.VBox;
 
-public class AddGameFrame extends VBox {
+import java.util.Objects;
 
-    private Frame frame;
+public class AddGameFrame extends ScrollPane {
+
+    private final Frame frame;
 
     public AddGameFrame(Frame frame) {
         this.frame = frame;
         setId("container");
-        getChildren().addAll(
+        VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.getChildren().addAll(
                 createGameTile(new MinecraftMainFrame(), "Minecraft", "/icons/gameSections/minecraft.jpg"),
                 createGameTile(new FactorioMainFrame(), "Factorio", "/icons/gameSections/factorio.png"),
                 createGameTile(new SatisfactoryMainFrame(), "Satisfactory", "/icons/gameSections/satisfactory.png"),
                 createGameTile(new RimworldMainFrame(), "Rimworld", "/icons/gameSections/Rimworld.png")
         );
+        setContent(vbox);
+        viewportBoundsProperty().addListener((observableValue, bounds, t1) -> {
+            Node content = getContent();
+            setFitToWidth(content.prefWidth(-1) < t1.getWidth());
+            setFitToHeight(content.prefHeight(-1) < t1.getHeight());
+        });
     }
 
     private Node createGameTile(Node node, String title, String iconPath) {
@@ -39,14 +49,15 @@ public class AddGameFrame extends VBox {
         BorderPane sidePane = new BorderPane();
         Label labelTitle = new Label(title);
         JFXTextField saveLocation = new JFXTextField();
-        setId("container-tile");
+        pane.setId("container-tile");
+        pane.setPrefHeight(100);
 
         FlowPane sectionImage = new FlowPane(Orientation.VERTICAL);
         sectionImage.setColumnHalignment(HPos.CENTER);
         sectionImage.setAlignment(Pos.CENTER);
         sectionImage.setMinWidth(100);
 
-        ImageView imageView = new ImageView(new Image(getClass().getResource(iconPath).toString()));
+        ImageView imageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResource(iconPath)).toString()));
         double imageWidth = 40;
         imageView.setFitHeight(imageWidth);
         imageView.setFitWidth(imageWidth);
@@ -78,18 +89,6 @@ public class AddGameFrame extends VBox {
         pane.setLeft(sidePane);
         pane.setCenter(saveLocation);
 
-        /*createDebugBox(sectionImage, "purple");
-        createDebugBox(imageView, "yellow");
-        createDebugBox(pane, "blue");
-        createDebugBox(labelTitle, "magenta");
-        createDebugBox(sidePane, "red");
-        createDebugBox(saveLocation, "brown");*/
-
         return pane;
     }
-
-    private void createDebugBox(Node node, String color) {
-        node.setStyle("-fx-border-color:"+color);
-    }
-
 }

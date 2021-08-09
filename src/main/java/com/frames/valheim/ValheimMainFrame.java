8e49efa72718beda814eae3ded841aa808e4e83f
Ben Teppers <ben.teppers@icloud.com>
2021-08-09 23:05:24 +0200

    Valheim and Sims4 added
    Both need to be checked and tested in following commits

---
+++ src/main/java/com/frames/valheim/ValheimMainFrame.java
@@ -0,0 +1,9 @@
+package com.frames.valheim;
+
+import com.frames.core.TabFrame;
+
+public class ValheimMainFrame extends TabFrame {
+    public ValheimMainFrame() {
+        super("pathToValheimSaveFolder", "Valheim");
+    }
+}
